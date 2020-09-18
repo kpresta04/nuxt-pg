ad3ef10f77a44c37431b1207a4bd22c4ad067287
Kellen Presta <kpresta04@gmail.com>
2020-09-18 08:54:51 -0500

    functionality

---
+++ components/Joke.vue
@@ -0,0 +1,23 @@
+<template>
+  <nuxt-link :to="'jokes/' + id">
+    <div class="joke">
+      <p>{{ joke }}</p>
+    </div>
+  </nuxt-link>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'Joke',
+  props: ['joke', 'id'],
+})
+</script>
+
+ <style  scoped>
+.joke {
+  padding: 1rem;
+  border: 1px dotted #ccc;
+  margin: 1rem 0;
+}
+</style>
