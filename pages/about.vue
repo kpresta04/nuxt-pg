24dfa25610384dac49d46767e1aa94f2b9f9ec47
Kellen Presta <kpresta04@gmail.com>
2020-09-18 05:06:59 -0500

    init

---
+++ pages/about.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>About Dad Jokes</h1>
+    <p>This is an app that displays jokes</p>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
